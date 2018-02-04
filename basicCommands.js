ad151b38dab77212d674a77f39a4c127abf8bcd1
Nick Redshaw <nickredshaw@gmail.com>
2018-02-04 20:37:03 +0000

    Add more examples to the basic commands module

--- basicCommands.js
+++ basicCommands.js
@@ -28,20 +28,39 @@ function doubleIt(x) {
 }
 console.log('Five times two is: ' + doubleIt(5));
 
-// Arrays and the map function
+// =======
+// Arrays
+// =======
 const animals = ['Dog', 'Cat', 'Mouse'];
 console.log(animals);
+console.log(animals[0]);
+console.log(animals[1]);
+console.log(animals[2]);
+console.log(animals.length);
 
-animals.map(function (animal) {
+animals.forEach(function (animal) {
   console.log(animal);
 });
 
 // Or using ES6 'arrow functions'
-animals.map((animal) => {
+animals.forEach(animal => {
   console.log(animal);
 });
 
+// Append / remove to / from the array
+animals.push('Horse');
+console.log(animals);
+
+const removedAnimal = animals.pop();
+console.log(animals);
+console.log('We removed a ' + removedAnimal)
+
+animals[0] = 'Wolf'
+console.log(animals);
+
+// ===========
 // Conditions
+// ===========
 const man = 'John';
 if (man === 'John') {
   console.log('The name was John');
@@ -55,10 +74,42 @@ if (man === 'Jim') {
   console.log('The name was NOT Jim');
 }
 
+// ========
 // Objects
+// ========
 const person = {
   firstName: 'John',
-  lastName: 'Smith'
+  lastName: 'Smith',
+  age: 42
 };
+console.log(person)
 console.log('First name is ' + person.firstName);
 console.log('Last name is ' + person.lastName);
+console.log('Age is ' + person.age);
+
+// Adding properties
+person.mobileNumber = '0123 456789'
+console.log('Mobile number is ' + person.mobileNumber);
+
+person.mobileNumber = ['0123 456789', '0987 654321'];
+console.log('Mobile numbers are ' + person.mobileNumber);
+
+// Get the keys
+console.log("Keys " + Object.keys(person));
+
+// Arrays of objects
+const people = [
+  {
+    firstName: 'John',
+    lastName: 'Smith',
+    age: 42
+  },
+  {
+    firstName: 'Joe',
+    lastName: 'Bloggs',
+    age: 29
+  }
+];
+people.forEach((person) => {
+  console.log("Person " + JSON.stringify(person))
+})
