ee7c50760fbaeb5c506c761aeabd7780f8dcb040
Nick Redshaw <nickredshaw@gmail.com>
2018-02-12 15:44:23 +0000

    New fizz buzz starter file added ready for workshop

---
+++ test/myFizzBuzz.spec.js
@@ -0,0 +1,8 @@
+const assert = require('chai').assert;
+const game = require('../application/myFizzBuzzGame');
+
+// assert example: assert.strictEqual(game.say(1), '1');
+
+describe('My Fizz Buzz Tests', () => {
+
+});
